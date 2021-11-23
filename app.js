acdf29a617f03bee3a1ea24e256fda05a9eab189
hasan <hasandemir.5802@gmail.com>
2021-11-24 01:43:59 +0300

    Geliştirildi

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ app.set("view engine","ejs")
 
 const data = request('GET', 'https://api.trendyol.com/sapigw/product-categories');
 let datau=JSON.parse(data.getBody('utf8'))
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req,res)=>{
     
     res.render('index',{
         datau
+    })
+})
+app.get('/ozellik', (req,res)=>{
+   
+    
+    let oz = request('GET',`https://api.trendyol.com/sapigw/product-categories/${req.query.id}/attributes`);
+    let ozu=JSON.parse(oz.getBody('utf8'))
+    res.render('ozellik',{ozu
     })
 })
 
